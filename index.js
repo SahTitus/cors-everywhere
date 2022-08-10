dd4aa8b1706aafae2a36e5a69a4b7983ea6aca8d
SahTitus <sahtitus58@gmail.com>
2022-08-11 03:15:12 +1200

    everywhere

---
+++ index.js
@@ -0,0 +1,14 @@
+const corsProxy = require('cors-anywhere');
+
+// Listen on a specific host via the HOST environment variable.
+const host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable.
+const port = process.env.PORT || 7300;
+
+corsProxy.createServer({
+    originWhitelist: ['http://localhost:3000', 'https://sm-downloader.vercel.app'],
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, () => {
+    console.log('Running CORS Anywhere on PORT ' + host + ':' + port);
+});
